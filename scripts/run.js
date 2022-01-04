21d89901cc0d2cec08d65fcb7a567630a31ca014
chen <peter-chen@rantion.com>
2022-01-04 11:27:18 +0800

    feature: add character

--- scripts/run.js
+++ scripts/run.js
@@ -1,13 +1,22 @@
 /*
  * @Author: Chan
  * @Date: 2022-01-04 11:06:59
- * @LastEditTime: 2022-01-04 11:07:00
+ * @LastEditTime: 2022-01-04 11:24:17
  * @LastEditors: Chan
  * @Description:
  */
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Leo", "Aang", "Pikachu"], // Names
+    [
+      "https://i.imgur.com/pKd5Sdk.png", // Images
+      "https://i.imgur.com/xVu4vFL.png",
+      "https://i.imgur.com/WMB6g9u.png"
+    ],
+    [100, 200, 300], // HP values
+    [100, 50, 25] // Attack damage values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
