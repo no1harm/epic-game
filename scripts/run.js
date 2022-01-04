00a9f7c1a3446d4e33a6f8890bf05fd6ac236f4b
chen <peter-chen@rantion.com>
2022-01-04 14:30:14 +0800

    feature: deploy rinbeky

--- scripts/run.js
+++ scripts/run.js
@@ -1,43 +1,33 @@
 /*
  * @Author: Chan
  * @Date: 2022-01-04 11:06:59
- * @LastEditTime: 2022-01-04 14:25:51
+ * @LastEditTime: 2022-01-04 14:29:17
  * @LastEditors: Chan
  * @Description:
  */
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
   const gameContract = await gameContractFactory.deploy(
-    ["Leo", "Aang", "Pikachu"],
+    ["Leo", "Aang", "Pikachu"], // Names
     [
-      "https://i.imgur.com/pKd5Sdk.png",
+      "https://i.imgur.com/pKd5Sdk.png", // Images
       "https://i.imgur.com/xVu4vFL.png",
-      "https://i.imgur.com/u7T87A6.png"
+      "https://i.imgur.com/WMB6g9u.png"
     ],
-    [100, 200, 300],
-    [100, 50, 25]
+    [100, 200, 300], // HP values
+    [100, 50, 25] // Attack damage values
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
-
   let txn;
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log("Minted NFT #1");
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #2");
-
+  // We only have three characters.
+  // an NFT w/ the character at index 2 of our array.
   txn = await gameContract.mintCharacterNFT(2);
   await txn.wait();
-  console.log("Minted NFT #3");
 
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #4");
-
-  console.log("Done deploying and minting!");
+  // Get the value of the NFT's URI.
+  let returnedTokenUri = await gameContract.tokenURI(1);
+  console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
